e54ca42adc39fe3d970d4e3e2ac6acd7a524379a
Muhammad Rizal <rizal@ruangguru.com>
2024-09-13 21:27:07 +0700

    fix eror

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Button } from "../components/Button";
 import { useUsers } from "../modules/user/useUsers";
 
